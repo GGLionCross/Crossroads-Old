f8fe77fa1b4d533e576a20c9856c46a669c2b65e
Lionel Quiambao <lionelquiambao@gmail.com>
2021-07-08 15:07:31 -0700

    Abstracted drawer menu items

--- src/components/DrawerMenu.vue
+++ src/components/DrawerMenu.vue
@@ -1,33 +1,29 @@
 <template>
-  <q-drawer v-bind="$attrs">
+  <q-drawer v-bind="$attrs" class="bg-drawer column justify-between">
+    <login-register />
     <q-list>
-      <q-item clickable>
-        <q-item-section avatar>
-          <q-icon name="loop"></q-icon>
-        </q-item-section>
-        <q-item-section>
-          <q-item-label>Reset</q-item-label>
-        </q-item-section>
-      </q-item>
-      <q-item clickable>
-        <q-item-section avatar>
-          <q-icon name="filter_alt"></q-icon>
-        </q-item-section>
-        <q-item-section>
-          <q-item-label>Filter</q-item-label>
-        </q-item-section>
-      </q-item>
+      <drawer-menu-item label="Reset" icon="loop" @click="resetCrossroads" />
+      <drawer-menu-item label="Filter" icon="filter_alt" />
     </q-list>
   </q-drawer>
 </template>
 
 <script>
-import { defineComponent } from 'vue';
+import { defineComponent } from "vue";
+import { useStore } from "vuex";
+import DrawerMenuItem from "./DrawerMenuItem.vue";
+import LoginRegister from "./LoginRegister.vue";
 
 export default defineComponent({
-  name: "DrawerMenu",
+  components: {
+    DrawerMenuItem,
+    LoginRegister
+  },
   setup() {
-    return {};
+    const store = useStore();
+    return {
+      resetCrossroads: () => store.dispatch('resetCrossroads')
+    };
   }
 })
 </script>
---
+++ src/components/DrawerMenuItem.vue
@@ -0,0 +1,24 @@
+<template>
+  <q-item v-bind="$attrs" clickable>
+    <q-item-section avatar>
+      <q-icon :name="icon"></q-icon>
+    </q-item-section>
+    <q-item-section>
+      <q-item-label>{{ label }}</q-item-label>
+    </q-item-section>
+  </q-item>
+</template>
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  props: {
+    label: {
+      type: String
+    },
+    icon: {
+      type: String
+    }
+  }
+});
+</script>

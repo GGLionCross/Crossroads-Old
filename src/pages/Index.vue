7e90ff7fde73b7bce9e22249463c45ef24b22abe
Lionel Quiambao <lionelquiambao@gmail.com>
2021-07-08 15:09:22 -0700

    Got rid of redundant name attribute

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -25,7 +25,6 @@ import { useStore } from 'vuex';
 import CrossroadsCard from 'components/CrossroadsCard.vue';
 
 export default defineComponent({
-  name: 'PageIndex',
   components: {
     CrossroadsCard
   },

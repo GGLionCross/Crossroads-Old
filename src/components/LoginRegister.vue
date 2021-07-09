26bd9338028e123c07baa7e92ce747200b5019ec
Lionel Quiambao <lionelquiambao@gmail.com>
2021-07-09 08:47:43 -0700

    Added vuelidate vue 3 alpha

--- src/components/LoginRegister.vue
+++ src/components/LoginRegister.vue
@@ -24,9 +24,24 @@
       leave-active-class="animated fadeOut"
     >
       <q-tab-panels v-if="!isClosed" v-model="tab" animated class="bg-drawer">
-        <q-tab-panel name="login" class="q-gutter-y-sm">
-          <q-input label="Email" color="yellow-14" dark dense outlined></q-input>
-          <q-input label="Password" color="yellow-14" dark dense outlined></q-input>
+        <q-tab-panel name="login">
+          <q-input
+            v-model="vv.loginInfo.username.$model"
+            :error="vv.loginInfo.username.$error"
+            label="Username"
+            color="yellow-14"
+            dark
+            dense
+            outlined
+          ></q-input>
+          <q-input
+            v-model="vv.loginInfo.password.$model"
+            :error="vv.loginInfo.password.$error"
+            label="Password"
+            color="yellow-14"
+            dark dense
+            outlined
+          ></q-input>
           <div class="row justify-end">
             <q-btn
               color="red-10"
@@ -36,11 +51,34 @@
             ></q-btn>
           </div>
         </q-tab-panel>
-        <q-tab-panel name="register" class="q-gutter-y-sm">
-          <q-input label="Email" color="yellow-14" dark dense outlined></q-input>
-          <q-input label="Name" color="yellow-14" dark dense outlined></q-input>
-          <q-input label="Password" color="yellow-14" dark dense outlined></q-input>
-          <q-input label="Confirm Password" color="yellow-14" dark dense outlined></q-input>
+        <q-tab-panel name="register">
+          <q-input
+            v-model="vv.registerInfo.username.$model"
+            :error="vv.registerInfo.username.$error"
+            label="Username"
+            color="yellow-14"
+            dark
+            dense
+            outlined
+          ></q-input>
+          <q-input
+            v-model="vv.registerInfo.password.$model"
+            :error="vv.registerInfo.password.$error"
+            label="Password"
+            color="yellow-14"
+            dark
+            dense
+            outlined
+          ></q-input>
+          <q-input
+            v-model="vv.registerInfo.confirm.$model"
+            :error="vv.registerInfo.confirm.$error"
+            label="Confirm Password"
+            color="yellow-14"
+            dark
+            dense
+            outlined
+          ></q-input>
           <div class="row justify-end">
             <q-btn
               color="red-10"
@@ -58,6 +96,8 @@
 <script>
 import { computed, defineComponent, ref } from "vue";
 import { useStore } from "vuex";
+import { useVuelidate } from "@vuelidate/core";
+import { required } from "@vuelidate/validators";
 
 export default defineComponent({
   setup() {
@@ -69,23 +109,44 @@ export default defineComponent({
     const closeLoginRegister = () => { tab.value = '' };
 
     const loginInfo = ref({
-      email: '',
+      username: '',
       password: ''
     });
     const registerInfo = ref({
-      email: '',
-      name: '',
+      username: '',
       password: '',
       confirm: ''
     });
 
     const store = useStore();
     const registerUser = () => { store.dispatch('registerUser') };
+
+    const rules = {
+      loginInfo: {
+        username: { required },
+        password: { required }
+      },
+      registerInfo: {
+        username: { required },
+        password: { required },
+        confirm: { required }
+      }
+    }
+    const vv = useVuelidate(rules, {
+      loginInfo,
+      registerInfo
+    });
+
+    console.error("vv:", vv.value);
+
     return {
       tab,
       isClosed,
       closeLoginRegister,
-      registerUser
+      registerUser,
+      loginInfo,
+      registerInfo,
+      vv
     }
   }
 });

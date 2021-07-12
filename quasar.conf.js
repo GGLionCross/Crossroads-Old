b68e3aa794fd4e5ab3f9c43f09733a93a648377d
Lionel Quiambao <lionelquiambao@gmail.com>
2021-07-12 14:06:00 -0700

    Removed global-components.js from boot

--- quasar.conf.js
+++ quasar.conf.js
@@ -22,8 +22,7 @@ module.exports = configure(function (ctx) {
     // --> boot files are part of "main.js"
     // https://v2.quasar.dev/quasar-cli/boot-files
     boot: [
-      'firebase',
-      'global-components'
+      'firebase'
     ],
 
     // https://v2.quasar.dev/quasar-cli/quasar-conf-js#Property%3A-css
--- src/boot/global-components.js
+++
@@ -1,7 +0,0 @@
-import { createApp } from "vue";
-import BaseInput from "src/components/BaseComponents/BaseInput.vue";
-
-// Called within .quasar/app.js
-export function initializeGlobalComponents(app) {
-  app.component("base-input", BaseInput);
-}

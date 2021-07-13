44cfd6ec07bbd162f68c814e73195de7ee5c4802
Lionel Quiambao <lionelquiambao@gmail.com>
2021-07-13 13:12:55 -0700

    Removed closing of Login/Register

--- src/components/LoginRegister.vue
+++ src/components/LoginRegister.vue
@@ -1,91 +1,71 @@
 <template>
   <q-card class="bg-drawer overflow-hidden" flat>
-    <div class="row">
-      <q-tabs v-model="tab" class="col-grow">
-        <q-tab name="login" label="Login" />
-        <q-tab name="register" label="Register" />
-      </q-tabs>
-      <transition
-        appear
-        enter-active-class="animated slideInRight"
-        leave-active-class="animated slideOutRight"
-      >
-        <q-btn
-          v-if="!isClosed"
-          icon="close"
-          flat
-          @click="closeLoginRegister"
-        />
-      </transition>
-    </div>
-    <transition
-      appear
-      enter-active-class="animated fadeIn"
-      leave-active-class="animated fadeOut"
-    >
-      <q-tab-panels v-if="!isClosed" v-model="tab" animated class="bg-drawer">
-        <q-tab-panel name="login" class="q-gutter-y-sm">
-          <base-input
-            v-model="v$.loginInfo.email.$model"
-            :error="v$.loginInfo.email.$error"
-            :error-message="getErrorMessage(v$.loginInfo.email.$errors)"
-            label="Email"
-          ></base-input>
-          <base-input
-            v-model="v$.loginInfo.password.$model"
-            :error="v$.loginInfo.password.$error"
-            :error-message="getErrorMessage(v$.loginInfo.password.$errors)"
-            label="Password"
-            type="password"
-          ></base-input>
-          <div class="row justify-end">
-            <q-btn
-              color="red-10"
-              icon="arrow_forward"
-              size="1em"
-              flat
-              @click="loginUser"
-            ></q-btn>
-          </div>
-        </q-tab-panel>
-        <q-tab-panel name="register" class="q-gutter-y-sm">
-          <base-input
-            v-model="v$.registerInfo.email.$model"
-            :error="v$.registerInfo.email.$error"
-            :error-message="getErrorMessage(v$.registerInfo.email.$errors)"
-            label="Email"
-          ></base-input>
-          <base-input
-            v-model.lazy="v$.registerInfo.username.$model"
-            :error="v$.registerInfo.username.$error"
-            :error-message="getErrorMessage(v$.registerInfo.username.$errors)"
-            label="Username"
-          ></base-input>
-          <base-input
-            v-model="v$.registerInfo.password.$model"
-            :error="v$.registerInfo.password.$error"
-            :error-message="getErrorMessage(v$.registerInfo.password.$errors)"
-            label="Password"
-            type="password"
-          ></base-input>
-          <base-input
-            v-model="registerInfo.confirm"
-            :rules="[value => passwordsMatch || 'Passwords don\'t match']"
-            label="Confirm Password"
-            type="password"
-          ></base-input>
-          <div class="row justify-end">
-            <q-btn
-              color="red-10"
-              icon="arrow_forward"
-              size="1em"
-              flat
-              @click="registerUser"
-            ></q-btn>
-          </div>
-        </q-tab-panel>
-      </q-tab-panels>
-    </transition>
+    <q-tabs v-model="tab" class="col-grow">
+      <q-tab name="login" label="Login" />
+      <q-tab name="register" label="Register" />
+    </q-tabs>
+    <q-tab-panels v-if="!isClosed" v-model="tab" animated class="bg-drawer">
+      <q-tab-panel name="login" class="q-gutter-y-sm">
+        <base-input
+          v-model="v$.loginInfo.email.$model"
+          :error="v$.loginInfo.email.$error"
+          :error-message="getErrorMessage(v$.loginInfo.email.$errors)"
+          label="Email"
+        ></base-input>
+        <base-input
+          v-model="v$.loginInfo.password.$model"
+          :error="v$.loginInfo.password.$error"
+          :error-message="getErrorMessage(v$.loginInfo.password.$errors)"
+          label="Password"
+          type="password"
+        ></base-input>
+        <div class="row justify-end">
+          <q-btn
+            color="red-10"
+            icon="arrow_forward"
+            size="1em"
+            flat
+            @click="loginUser"
+          ></q-btn>
+        </div>
+      </q-tab-panel>
+      <q-tab-panel name="register" class="q-gutter-y-sm">
+        <base-input
+          v-model="v$.registerInfo.email.$model"
+          :error="v$.registerInfo.email.$error"
+          :error-message="getErrorMessage(v$.registerInfo.email.$errors)"
+          label="Email"
+        ></base-input>
+        <base-input
+          v-model.lazy="v$.registerInfo.username.$model"
+          :error="v$.registerInfo.username.$error"
+          :error-message="getErrorMessage(v$.registerInfo.username.$errors)"
+          label="Username"
+        ></base-input>
+        <base-input
+          v-model="v$.registerInfo.password.$model"
+          :error="v$.registerInfo.password.$error"
+          :error-message="getErrorMessage(v$.registerInfo.password.$errors)"
+          label="Password"
+          type="password"
+        ></base-input>
+        <base-input
+          v-model="registerInfo.confirm"
+          :rules="[value => passwordsMatch || 'Passwords don\'t match']"
+          label="Confirm Password"
+          type="password"
+        ></base-input>
+        <div class="row justify-end">
+          <q-btn
+            color="red-10"
+            icon="arrow_forward"
+            size="1em"
+            flat
+            @click="registerUser"
+          ></q-btn>
+        </div>
+      </q-tab-panel>
+    </q-tab-panels>
   </q-card>
 </template>
 <script>
@@ -108,7 +88,7 @@ export default defineComponent({
     /* tab can either be "", "login", "register"
      * tab = "" represents the "closed" state
      */
-    const tab = ref("");
+    const tab = ref("login");
     const isClosed = computed(() => tab.value === "");
     const closeLoginRegister = () => { tab.value = "" };
 

0ef6fb6a2a9f0afeaa5f8f86f5d4c2f2c3e81121
Lionel Quiambao <lionelquiambao@gmail.com>
2021-07-13 11:42:47 -0700

    Added mutation for setting currentUser

--- src/store/mutations.js
+++ src/store/mutations.js
@@ -1,6 +1,9 @@
 export function setCounter (state, i) {
   state.counter = i;
 }
+export function setCurrentUser (state, user) {
+  state.currentUser = user;
+}
 export function setFilteredCards (state, cards) {
   state.filteredCards = cards;
 }

fc90f1ed0651dd8df01d8a4f3751a70119bdf7c1
Lionel Quiambao <lionelquiambao@gmail.com>
2021-07-14 10:42:06 -0700

    Reorg'd state

--- src/store/state.js
+++ src/store/state.js
@@ -1,9 +1,6 @@
 export default function () {
   return {
-    currentUser: {},
-    counter: 0,
-    filteredCards: [],
-    // special: bandits, character specific, improvements, raxxon
+    // special: bandits, character specific, improvements, raxxon, warring_colonies
     cards: {
       // template: {
       //   name: '',
@@ -18,7 +15,6 @@ export default function () {
       //   ]
       // },
       army: {
-        use: true,
         name: 'Army',
         trigger: 'If a survivor the player controls is at the colony:',
         intro: 'The soldiers in army uniforms make predictable demands - have food ready when they return, or the colony will be burnt to the ground. Every player with 1 or more survivors at the colony must vote with a thumbs up or down. The option with most votes takes effect. The ALTERNATIVE option triggers if Thomas Heart is at the colony.',
@@ -41,7 +37,6 @@ export default function () {
         ]
       },
       ill_show_them_i_can_help: {
-        use: true,
         name: 'I\'ll Show Them I Can Help!',
         trigger: 'If a player is accused of being a betrayer (a casual accusation is enough) and there is at least 1 helpless survivor at the colony:',
         intro: 'Once again, a loud mouth is accusing someone else of betrayal. However, the next morning, the accused is found dead."Who did this?!" you demand. "Me," says a meek little girl in the crowd. "I... I saved everybody... Didn\'t I?" Kill the leader of the player that was being accused of being a betrayer. Every non-exiled player must vote with a thumbs up or down.',
@@ -64,7 +59,6 @@ export default function () {
         ]
       },
       oh_what_a_feeling: {
-        use: true,
         name: 'Oh What A Feeling...',
         trigger: 'If a human survivor the player controls shares a location a human survivor another player controls and those are the only 2 survivors at that location:',
         intro: 'The storm is unexpected and so is the company. "We should probably share a blanket, right?" you ask awkwardly. You don\'t know each other very well, but the time has passed pleasantly so far. Your hands brush under the blanket. Your eyes lock. Both players that control a survivor at that location must vote on the count of 3 with a thumbs up or down.',
@@ -87,7 +81,6 @@ export default function () {
         ]
       },
       riot: {
-        use: true,
         name: 'Riot',
         trigger: 'If a player controls a survivor at the colony, there are more helpless survivors in the colony than non-helpless survivors, and there is food in the supply:',
         intro: '"They don\'t care that we\'re starving! I can feel my ribs, yet the pantry door is locked up tight. Why? We need food. Well I don\'t care what those scum bags say - there\'s way more of us than them. I say we break that door down, and if any of them get in our way, well... it\'s their choice if things get ugly."',
@@ -109,6 +102,9 @@ export default function () {
           }
         ]
       }
-    }
+    },
+    counter: 0,
+    currentUser: {},
+    filter: []
   }
 }

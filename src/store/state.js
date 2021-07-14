e0a58af9b01232f0be276ae75285bb56ca9c4fcb
Lionel Quiambao <lionelquiambao@gmail.com>
2021-07-14 15:53:52 -0700

    Added more cards to db import file

--- src/store/state.js
+++ src/store/state.js
@@ -1,19 +1,31 @@
 export default function () {
   return {
-    // special: bandits, character specific, improvements, raxxon, warring_colonies
+    // Tags:
+    // basic (None of the other tags)
+    // bandits (Bandits Module)
+    // changed (Notes about changes made to the card that fundamentally alter gameplay)
+    // character (Requires specific character to be triggered)
+    // explicit (Contains explicit themes/language)
+    // improvements (Improvements Module)
+    // persistent (Has persisting effect / a physical copy is needed)
+    //   Set this card aside to remember this persisting effect.
+    // raxxon (Raxxon Module)
+    // warring (Warring Colonies Expansion)
     cards: {
-      // template: {
-      //   name: '',
-      //   trigger: '',
-      //   intro: '',
-      //   options: [
+      // "template": {
+      //   "key": "template",
+      //   "name": "",
+      //   "trigger": ":",
+      //   "intro": "",
+      //   "options": [
       //     {
-      //       label: '',
-      //       subtext: '',
-      //       result: ''
+      //       "label": "",
+      //       "subtext": "",
+      //       "result": ""
       //     }
-      //   ]
-      // },
+      //   ],
+      //   "basic": true
+      // }
     },
     counter: 0,
     currentUser: {},
